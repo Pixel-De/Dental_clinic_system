2d2be16d6c4200a46142a30737557bfedc1de6e7
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2021-05-09 17:07:50 +0800

    doctorList

--- src/DentalClinic/Patient/InformationController.java
+++ src/DentalClinic/Patient/InformationController.java
@@ -1,4 +1,61 @@
 package DentalClinic.Patient;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
+//import java.awt.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 public class InformationController {
+
+    @FXML
+    TextField id, name, parent, address, contact, age, occupation;
+//    @FXML
+//    ChoiceBox<String> gender;
+//    @FXML
+//    DatePicker reference, date;
+
+    public InformationController(){
+
+    }
+    public InformationController(Patient p){
+//        id.setText(p.getId().toString());
+//        name.setText(p.getName());
+//        parent.setText(p.getParent());
+//        address.setText(p.getAddress());
+//        contact.setText(p.getContact().toString());
+//        age.setText(p.getAge().toString());
+//        occupation.setText(p.getOccupation());
+//        gender.setValue(p.getGender());
+//        LocalDate ref = p.getReference().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+//        reference.setValue(ref);
+//        LocalDate d = p.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+//        date.setValue(d);
+    }
+    public void initialize(){
+//        gender.getItems().add("Male");
+//        gender.getItems().add("Female");
+    }
+    public void showInformation(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./InformationView.fxml"));
+        Parent rootDocInfo = null;
+        try {
+            rootDocInfo = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootDocInfo));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/DentalClinic/Patient/ListController.java
+++ src/DentalClinic/Patient/ListController.java
@@ -3,13 +3,20 @@ package DentalClinic.Patient;
 import DentalClinic.DB.DbConnect;
 import com.mysql.cj.xdevapi.Table;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Date;
 
 public class ListController {
@@ -24,6 +31,15 @@ public class ListController {
             System.out.println("db got here");
             ObservableList<Patient> patients = data.PatientList();
             patients.forEach(patient -> {
+                Button b = patient.getEdit();
+                b.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+
+                        InformationController ic = new InformationController(patient);
+                        ic.showInformation();
+                    }
+                });
                 System.out.println(patient.getId()+" "+patient.getName());
             });
 
