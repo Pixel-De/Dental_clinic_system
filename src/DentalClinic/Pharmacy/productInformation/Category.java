e384a999b4084139611fe83fb3b79919d0b4f1ab
Khenbai <hegii2001@gmail.com>
2021-05-12 23:16:55 +0800

    category added without id and return id

--- src/DentalClinic/Pharmacy/productInformation/Category.java
+++ src/DentalClinic/Pharmacy/productInformation/Category.java
@@ -9,6 +9,14 @@ public class Category {
     private String category;
     private Integer id;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getCategory() {
         return category;
     }
