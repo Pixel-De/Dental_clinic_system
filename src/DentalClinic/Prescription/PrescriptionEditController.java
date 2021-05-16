868b9b9d2d8720fd86afba6e911651fa7eea9769
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2021-05-17 04:53:44 +0800

    yueyue

--- src/DentalClinic/Prescription/PrescriptionEditController.java
+++ src/DentalClinic/Prescription/PrescriptionEditController.java
@@ -1,5 +1,8 @@
 package DentalClinic.Prescription;
 
+import DentalClinic.DB.DbConnect;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -17,13 +20,24 @@ public class PrescriptionEditController {
     @FXML
     TableView<PrescriptionFull> preTableView;
 
+    ObservableList<PrescriptionFull> prescriptionFullList = FXCollections.observableArrayList();
+    DbConnect  db = new DbConnect();
+
 
     public PrescriptionEditController(PrescriptionFull prescriptionFull){
 
         preIdField.setText(prescriptionFull.getId());
         preIdField.setEditable(false);
         patientNameField.setText(prescriptionFull.getName());
+        patientNameField.setEditable(false);
         ageField.setText(prescriptionFull.getAge());
+        ageField.setEditable(false);
+        genderField.setText(prescriptionFull.getGender());
+        genderField.setEditable(false);
+
+//        prescriptionFullList = db.G
+
+
 
 
 
