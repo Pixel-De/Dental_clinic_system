4eb3acaf730cfc9f57ed29c16e79b0521d578e2f
covidslayer69 <xacap46@gmail.com>
2021-05-18 18:24:30 +0800

    FIxed USERINFOCONT

--- src/DentalClinic/LoginController.java
+++ src/DentalClinic/LoginController.java
@@ -1,15 +1,24 @@
 package DentalClinic;
 
+import DentalClinic.Administration.ListOfUser.User;
+import DentalClinic.Administration.Permission.PermissionModel;
+import DentalClinic.DB.DbConnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
+import DentalClinic.LoginModel;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class LoginController {
 
@@ -28,33 +37,48 @@ public class LoginController {
     @FXML
     private ImageView imageView;
 
-    @FXML
-    void closeWindow(ActionEvent event) {
-        Stage stage = (Stage) btnClose.getScene().getWindow();
-        stage.close();
-    }
+    private DbConnect db = new DbConnect();
 
     @FXML
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        txtUsername.getText();
+//        txtPassword.getText();
+//
+//    }
     void submitData(ActionEvent event) {
         String username = txtUsername.getText(), password = txtPassword.getText();
+        System.out.println(username + password);
+        PermissionModel permissionmodel = db.Login(username, password);
+        if (permissionmodel != null) {
+            System.out.println(permissionmodel.toString());
 
-        if(username.length() > 0 && password.length() > 0){
+        }
+        else {
+            System.out.println("iim hereglegch baihgui");
+        }
+        if (username.length() > 0 && password.length() > 0) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Success!");
             alert.setHeaderText(null);
             alert.setContentText("Successfully logged in!");
 
-            alert.showAndWait();
-        }
-        else{
+
+        } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error!");
             alert.setHeaderText(null);
             alert.setContentText("Please fill the all fields!");
 
             alert.showAndWait();
+            }
         }
 
+
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
     }
 
     public void initialize() throws  Exception{
