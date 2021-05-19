4514c8b95dcb764b7e42831fb4cc682aa1f13f8f
covidslayer69 <xacap46@gmail.com>
2021-05-19 17:06:59 +0800

    Conditioned mainMenucont

--- src/DentalClinic/MainMenu/MainMenuController.java
+++ src/DentalClinic/MainMenu/MainMenuController.java
@@ -1,6 +1,7 @@
 package DentalClinic.MainMenu;
 
 import DentalClinic.Administration.ListOfUser.ListOfUserController;
+import DentalClinic.Administration.Permission.PermissionModel;
 import DentalClinic.Administration.UserInformation.UserInfoController;
 import DentalClinic.Administration.UserInformation.UserInformationController;
 import javafx.event.ActionEvent;
@@ -8,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
@@ -19,6 +21,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainMenuController {
+
+    PermissionModel permissionModel ;
 
     @FXML
     private AnchorPane mainMenu;
@@ -56,13 +60,20 @@ public class MainMenuController {
     @FXML
     private Label UserRank;
 
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+
     @FXML
     void setClinicInformation(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/ClinicInformation/ClinicInformationView.fxml"));
-        Parent clinic = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(clinic));
-        stage.show();
+
+        if(permissionModel.getAdmin()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/ClinicInformation/ClinicInformationView.fxml"));
+            Parent clinic = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(clinic));
+            stage.show();
+        } else {
+            alert.showAndWait();
+        }
     }
 
     @FXML
@@ -72,11 +83,18 @@ public class MainMenuController {
 
     @FXML
     void setDoctorInformation(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Doctor/InfoView.fxml"));
-        Parent rootDocInfo = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootDocInfo));
-        stage.show();
+
+        if(permissionModel.getDoctor_info()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Doctor/InfoView.fxml"));
+            Parent rootDocInfo = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootDocInfo));
+            stage.show();
+        } else {
+            alert.showAndWait();
+        }
+
+
     }
 
     @FXML
@@ -86,150 +104,266 @@ public class MainMenuController {
 
     @FXML
     void setListOfDoctors(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Doctor/ListView.fxml"));
-        Parent rootDocList = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootDocList));
-        stage.show();
+
+        if(permissionModel.getDoctor_list()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Doctor/ListView.fxml"));
+            Parent rootDocList = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootDocList));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setListOfPatient(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Patient/ListView.fxml"));
-        Parent rootDocList = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootDocList));
-        stage.show();
+
+        if(permissionModel.getPatient_list()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Patient/ListView.fxml"));
+            Parent rootDocList = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootDocList));
+            stage.show();
+        }else {
+            alert.showAndWait();
+        }
     }
 
     @FXML
     void setListOfUsers(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/ListOfUser/ListOfUserView.fxml"));
-        Parent listofuser = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(listofuser));
-        stage.show();
+
+
+        if(permissionModel.getAdmin()){
+            ListOfUserController listUser = new ListOfUserController();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/ListOfUser/ListOfUserView.fxml"));
+            Parent listofuser = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(listofuser));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setListOfVoucher(ActionEvent event) throws  IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Income/ListOfVoucher.fxml"));
-        Parent patientsInfo = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(patientsInfo));
-        stage.show();
+        if(permissionModel.getVoucher_list()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Income/ListOfVoucher.fxml"));
+            Parent patientsInfo = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(patientsInfo));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPatientInformation(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Patient/InfoView.fxml"));
-        Parent patientsInfo = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(patientsInfo));
-        stage.show();
+
+        if(permissionModel.getPatient_info()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Patient/InfoView.fxml"));
+            Parent patientsInfo = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(patientsInfo));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPatientVisitList(ActionEvent event) throws IOException {
+
+//        if(permissionModel.get){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../Treatment/PatientVisitView.fxml"));
         Parent rootTreatVis = loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(rootTreatVis));
         stage.show();
+
+//        } else {
+//            alert.showAndWait();
+//        }
+
     }
 
     @FXML
     void setPermission(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/Permission/PermissionView.fxml"));
-        Parent permissione = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(permissione));
-        stage.show();
+
+        if(permissionModel.getAdmin()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/Permission/PermissionView.fxml"));
+            Parent permissione = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(permissione));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPharmacyReport(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/Report.fxml"));
-        Parent rootPharma = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharma));
-        stage.show();
+
+        if(permissionModel.getPharmacy_report()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/Report.fxml"));
+            Parent rootPharma = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharma));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPharmacySales(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/Sale/SaleView.fxml"));
-        Parent rootPharmaSale = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharmaSale));
-        stage.show();
+
+        if(permissionModel.getSales()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/Sale/SaleView.fxml"));
+            Parent rootPharmaSale = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharmaSale));
+            stage.show();
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPrescription(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Prescription/Presicription.fxml"));
-        Parent rootPharmaSales = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharmaSales));
-        stage.show();
+
+        if(permissionModel.getPrescription()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Prescription/Presicription.fxml"));
+            Parent rootPharmaSales = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharmaSales));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setPrescriptionList(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Prescription/PreList.fxml"));
-        Parent rootPharmaSaled = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharmaSaled));
-        stage.show();
+
+        if(permissionModel.getPrescription_list()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Prescription/PreList.fxml"));
+            Parent rootPharmaSaled = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharmaSaled));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
 
     }
 
     @FXML
     void setProductInformation(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/productInformation/ProductInfoView.fxml"));
-        Parent rootPharmaProductInfo = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharmaProductInfo));
-        stage.show();
+
+        if(permissionModel.getPharmacy_info()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/productInformation/ProductInfoView.fxml"));
+            Parent rootPharmaProductInfo = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharmaProductInfo));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setProductList(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/productList/ProductList.fxml"));
-        Parent rootPharmaProductCat = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(rootPharmaProductCat));
-        stage.show();
+
+        if(permissionModel.getPharmacy_list()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Pharmacy/productList/ProductList.fxml"));
+            Parent rootPharmaProductCat = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rootPharmaProductCat));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setTreatmentReports(ActionEvent event) throws IOException {
+//        if(permissionModel.get){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../Treatment/Report.fxml"));
         Parent rootTreatList = loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(rootTreatList));
         stage.show();
+
+//        } else {
+//            alert.showAndWait();
+//        }
     }
 
     @FXML
     void setUserInformation(ActionEvent event) throws IOException {
-        UserInfoController userinfocont = new UserInfoController();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/UserInformation/UserInfoView.fxml"));
-        loader.setController(userinfocont);
-        Parent clinics = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(clinics));
-        stage.show();
+
+        if(permissionModel.getAdmin()){
+            UserInfoController userinfocont = new UserInfoController();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Administration/UserInformation/UserInfoView.fxml"));
+            loader.setController(userinfocont);
+            Parent clinics = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(clinics));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
     void setVoucher(ActionEvent event)  throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Income/VoucherView.fxml"));
-        Parent clinicd = loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(clinicd));
-        stage.show();
+
+        if(permissionModel.getVoucher()){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Income/VoucherView.fxml"));
+            Parent clinicd = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(clinicd));
+            stage.show();
+
+        } else {
+            alert.showAndWait();
+        }
+
+
+    }
+
+    public MainMenuController(PermissionModel permissionModel){
+        this.permissionModel = permissionModel;
+//        System.out.println(permissionModel.getId());
+    }
+    public MainMenuController(){
 
     }
 
